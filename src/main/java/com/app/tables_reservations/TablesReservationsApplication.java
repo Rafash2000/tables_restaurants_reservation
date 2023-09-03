03dbcd188e6e42e0f8f49069b16e5d0ebcc91d24
Rafał <rafalbarylka@gmail.com>
2023-09-03 20:01:10 +0200

    init project

---
+++ src/main/java/com/app/tables_reservations/TablesReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.app.tables_reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablesReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablesReservationsApplication.class, args);
+	}
+
+}
